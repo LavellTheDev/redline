45ba97b1236832f7e87fd686febb2cdf7e1dbae1
Lavell Wesley <lavellthedev+github@gmail.com>
2019-08-24 17:35:38 -0400

    php and contact form

--- contactform.php
+++ contactform.php
@@ -1,20 +1,28 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+
+$email_from = 'lavwes@gmail.com';
+
+$email_subject = "New Form Submission";
+
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                  "User Message: $message.\n";
+
+
+$to = "lavwes@gmail.com";
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply To: $visitor_email \r\n";
+
+mail($to,email_subject,$email_body,$headers);
+
+header("Location: index.html");
 
 
 
- <?php
-
- if (isset($_POST['submit'])) {
- $name = $_POST['name'];
- $subject = $_POST['subject'];
- $mailFrom = $_POST['mail'];
- $message = $_POST['message'];
-
- $mailTo = "lavwes@gmail.com";
- $headers = "From: ".$mailFrom;
- $txt = "You have received an e-mail from ".$name.".\n\n".$message;
-
- mail($mailTo, $subject, $txt, $headers);
- header("Location: index.html");
- }
-
- ?>
+?>
